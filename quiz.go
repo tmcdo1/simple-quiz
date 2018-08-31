d6607163340c4eb99ec426149e17a613dc675445
Thomas McDonald <thomasmcdonald@Thomass-MacBook-Pro.local>
2018-08-31 18:30:22 -0500

    Added the time limit functionality using goroutines

--- quiz.go
+++ quiz.go
@@ -7,10 +7,12 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	fileName := flag.String("csv", "questions.csv", "the filename for the csv with the question,answer")
+	timeLimit := flag.Int("limit", -1, "Specify a time limit you would like to have for the quiz in seconds")
 	flag.Parse()
 
 	questionFile, err := os.Open(*fileName)
@@ -26,16 +28,36 @@ func main() {
 
 	numQuestions := len(records)
 	numCorrect := 0
-	for _, v := range records {
-		answer := strings.ToLower(strings.TrimSpace(v[1]))
-		fmt.Println(v[0])
-		var res string
-		fmt.Scanf("%s\n", &res)
-		res = strings.ToLower(strings.TrimSpace(res))
-		if res == answer {
-			numCorrect++
-		}
+
+	if *timeLimit > 0 {
+		tTicker := time.NewTicker(time.Duration(int64(*timeLimit)) * time.Second)
+		go func() {
+			<-tTicker.C
+			tTicker.Stop()
+			end(numCorrect, numQuestions)
+		}()
 	}
 
-	fmt.Printf("You go %d out of %d questions correctly!\n", numCorrect, numQuestions)
+	done := make(chan bool)
+	go func() {
+		for _, v := range records {
+			answer := strings.ToLower(strings.TrimSpace(v[1]))
+			fmt.Println(v[0])
+			var res string
+			fmt.Scanf("%s\n", &res)
+			res = strings.ToLower(strings.TrimSpace(res))
+			if res == answer {
+				numCorrect++
+			}
+		}
+		done <- true
+	}()
+
+	<-done
+	end(numCorrect, numQuestions)
+}
+
+func end(correct int, total int) {
+	fmt.Printf("You got %d out of %d questions correct!\n", correct, total)
+	os.Exit(0)
 }
